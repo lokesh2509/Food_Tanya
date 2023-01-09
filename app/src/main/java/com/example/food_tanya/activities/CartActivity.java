ea4a9142914854b393f6dab531dd7aea0aaf7e42
Lokesh Vyas <lokeshvyas904@gmail.com>
2023-01-09 21:24:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/food_tanya/activities/CartActivity.java
@@ -0,0 +1,16 @@
+package com.example.food_tanya.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.food_tanya.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
